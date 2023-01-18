0d6d19e086a4c7e555162ea0a72249a4903526ba
Eugenia Palamarenko <palamarenko.evgeniya@gmail.com>
2023-01-18 22:04:14 +0300

    add renderProduct.js,createCssColors.js

--- src/modules/render/renderProducts.js
+++ src/modules/render/renderProducts.js
@@ -1,177 +1,89 @@
-import product01 from '../../img/product01.jpg';
-import product02 from '../../img/product02.jpg';
 
-export const renderProducts = () => {
+import { API_URL, DATA } from '../const';
+import { createElement } from '../createElement';
+import { getData } from '../getData';
+
+export const renderProducts = async (title, params) => {
   const products = document.querySelector('.goods');
-  products.innerHTML = `
-  <div class="container">
-    <h2 class="goods__title">Новинки</h2>
+  products.textContent = '';
 
-    <ul class="goods__list">
-      <li class="goods__item">
-        <article class="product">
-          <a href="#" class="product__link">
-            <img src=${product01} alt="Бюстгальтер-Балконет Wien из Микрофибры" class="product__image">
-            <h3 class="product__title">Бюстгальтер-Балконет Wien из Микрофибры
-            </h3>
-          </a>
+  const goods = await getData(`${API_URL}/api/goods`, params);
 
-          <div class="product__row">
-            <p class="product__price">руб 2999</p>
-            <button class="product__btn-favorite" aria-label="добавить в избранное"></button>
-          </div>
+  const container = createElement('div', {
+    className: 'container'
+  }, {
+    parent: products
+  })
 
-          <ul class="product__color-list">
-            <li class="product__color-item">
-              <div class="color color_red color_check"></div>
-            </li>
-            <li class="product__color-item">
-              <div class="color color_white"></div>
-            </li>
-            <li class="product__color-item">
-              <div class="color color_black"></div>
-            </li>
-          </ul>
-        </article>
-      </li>
+  createElement('h2', {
+    className: 'goods__title',
+    textContent: title
+  }, {
+    parent: container
+  })
 
-      <li class="goods__item">
-        <article class="product">
-          <a href="#" class="product__link">
-            <img src=${product02} alt="Бюстгальтер-Балконет Wien из Микрофибры" class="product__image">
-            <h3 class="product__title">Бюстгальтер-Балконет Wien из Микрофибры
-            </h3>
-          </a>
+  const listCard = goods.map(product => {
+    const li = createElement('li', {
+      className: 'goods__item',
+    });
 
-          <div class="product__row">
-            <p class="product__price">руб 2999</p>
-            <button class="product__btn-favorite" aria-label="добавить в избранное"></button>
-          </div>
+    const article = createElement('article', {
+      className: 'product',
+      innerHTML: `
+        <a href="#/product/${product.id}" class="product__link">
+          <img src='${API_URL}/${product.pic}' alt="${product.title}" class="product__image">
+          <h3 class="product__title">${product.title}</h3>
+        </a>
 
-          <ul class="product__color-list">
-            <li class="product__color-item">
-              <div class="color color_red color_check"></div>
-            </li>
-            <li class="product__color-item">
-              <div class="color color_white"></div>
-            </li>
-            <li class="product__color-item">
-              <div class="color color_black"></div>
-            </li>
-          </ul>
-        </article>
-      </li>
+        <div class="product__row">
+          <p class="product__price">руб ${product.price}</p>
+          <button class="product__btn-favorite" 
+          aria-label="добавить в избранное"
+          data-id= ${product.id}>
+          </button>
+        </div>
+      `
+    }, {
+      parent: li
+    })
 
-      <li class="goods__item">
-        <article class="product">
-          <a href="#" class="product__link">
-            <img src=${product01} alt="Бюстгальтер-Балконет Wien из Микрофибры" class="product__image">
-            <h3 class="product__title">Бюстгальтер-Балконет Wien из Микрофибры
-            </h3>
-          </a>
+    const colors = createElement('ul', {
+      className: 'product__color-list'
+    }, {
+      parent: article,
+      appends: product.colors.map((colorId, i) => {
+        const color = DATA.colors.find(item => item.id == colorId);
 
-          <div class="product__row">
-            <p class="product__price">руб 2999</p>
-            <button class="product__btn-favorite" aria-label="добавить в избранное"></button>
-          </div>
+        return createElement('li', {
+          className: `color color_${color.title} ${i ? '' : 'color_check'}`
+        })
+      })
+    })
 
-          <ul class="product__color-list">
-            <li class="product__color-item">
-              <div class="color color_red color_check"></div>
-            </li>
-            <li class="product__color-item">
-              <div class="color color_white"></div>
-            </li>
-            <li class="product__color-item">
-              <div class="color color_black"></div>
-            </li>
-          </ul>
-        </article>
-      </li>
 
-      <li class="goods__item">
-        <article class="product">
-          <a href="#" class="product__link">
-            <img src=${product02} alt="Бюстгальтер-Балконет Wien из Микрофибры" class="product__image">
-            <h3 class="product__title">Бюстгальтер-Балконет Wien из Микрофибры
-            </h3>
-          </a>
 
-          <div class="product__row">
-            <p class="product__price">руб 2999</p>
-            <button class="product__btn-favorite" aria-label="добавить в избранное"></button>
-          </div>
+    return li;
+  })
 
-          <ul class="product__color-list">
-            <li class="product__color-item">
-              <div class="color color_red color_check"></div>
-            </li>
-            <li class="product__color-item">
-              <div class="color color_white"></div>
-            </li>
-            <li class="product__color-item">
-              <div class="color color_black"></div>
-            </li>
-          </ul>
-        </article>
-      </li>
+  const list = createElement('ul', {
+    className: 'goods__list',
+  }, {
+    appends: listCard,
+    parent: container
+  })
 
-      <li class="goods__item">
-        <article class="product">
-          <a href="#" class="product__link">
-            <img src=${product01} alt="Бюстгальтер-Балконет Wien из Микрофибры" class="product__image">
-            <h3 class="product__title">Бюстгальтер-Балконет Wien из Микрофибры
-            </h3>
-          </a>
 
-          <div class="product__row">
-            <p class="product__price">руб 2999</p>
-            <button class="product__btn-favorite" aria-label="добавить в избранное"></button>
-          </div>
 
-          <ul class="product__color-list">
-            <li class="product__color-item">
-              <div class="color color_red color_check"></div>
-            </li>
-            <li class="product__color-item">
-              <div class="color color_white"></div>
-            </li>
-            <li class="product__color-item">
-              <div class="color color_black"></div>
-            </li>
-          </ul>
-        </article>
-      </li>
+  // <ul class="product__color-list">
+  //   <li class="product__color-item">
+  //     <div class="color color_red color_check"></div>
+  //   </li>
+  //   <li class="product__color-item">
+  //     <div class="color color_white"></div>
+  //   </li>
+  //   <li class="product__color-item">
+  //     <div class="color color_black"></div>
+  //   </li>
+  // </ul>
 
-      <li class="goods__item">
-        <article class="product">
-          <a href="#" class="product__link">
-            <img src=${product02} alt="Бюстгальтер-Балконет Wien из Микрофибры" class="product__image">
-            <h3 class="product__title">Бюстгальтер-Балконет Wien из Микрофибры
-            </h3>
-          </a>
-
-          <div class="product__row">
-            <p class="product__price">руб 2999</p>
-            <button class="product__btn-favorite product__btn-favorite_active "
-              aria-label="добавить в избранное"></button>
-          </div>
-
-          <ul class="product__color-list">
-            <li class="product__color-item">
-              <div class="color color_red color_check"></div>
-            </li>
-            <li class="product__color-item">
-              <div class="color color_white"></div>
-            </li>
-            <li class="product__color-item">
-              <div class="color color_black"></div>
-            </li>
-          </ul>
-        </article>
-      </li>
-    </ul>
-  </div>
-
-  `
 }
